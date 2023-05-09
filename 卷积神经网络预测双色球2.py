ac64c6252b14343c78067a5ca20139d9089404f1
Kai Liu <59326332+lk569261093@users.noreply.github.com>
2023-05-09 18:57:42 +0800

    Add files via upload

---
+++ 卷积神经网络预测双色球2.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout, Bidirectional
+from tensorflow.keras.callbacks import EarlyStopping
+
+# 1. 读取数据
+data = pd.read_excel('data.xlsx')
+data = data.iloc[:, 1:9]  # 选择第二列到第八列的数据
+
+# 2. 数据预处理
+scaler = MinMaxScaler()
+data_scaled = scaler.fit_transform(data)
+
+def create_dataset(dataset, look_back=1):
+    X, Y = [], []
+    for i in range(len(dataset) - look_back):
+        a = dataset[i:(i + look_back), :]
+        X.append(a)
+        Y.append(dataset[i + look_back, :])
+    return np.array(X), np.array(Y)
+
+look_back = 10
+X, Y = create_dataset(data_scaled, look_back)
+
+# 3. 创建优化后的LSTM模型并训练
+model = Sequential()
+model.add(Bidirectional(LSTM(50, return_sequences=True), input_shape=(X.shape[1], X.shape[2])))
+model.add(Dropout(0.2))
+model.add(Bidirectional(LSTM(50)))
+model.add(Dropout(0.2))
+model.add(Dense(7))
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+
+model.fit(X, Y, epochs=200, batch_size=1, verbose=1, validation_split=0.1, callbacks=[early_stopping])
+
+# 4. 预测2023年5月9日的数据
+input_data = data_scaled[-look_back:]
+input_data = np.reshape(input_data, (1, input_data.shape[0], input_data.shape[1]))
+prediction = model.predict(input_data)
+predicted_values = scaler.inverse_transform(prediction)
+
+print("2023年5月9日的预测结果：")
+print(predicted_values)
